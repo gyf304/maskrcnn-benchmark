1ea3c491310ff44a7df08e4a5f26487d50cce787
Yifan Gu <gyf304@users.noreply.github.com>
2020-02-03 19:02:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 from setuptools import dist, find_packages, setup
 
-dist.Distribution().fetch_build_eggs(['Cython>=0.27.3', 'torch'])
+dist.Distribution().fetch_build_eggs(['Cython>=0.27.3', 'torch', 'ninja'])
 
 import torch
 from torch.utils.cpp_extension import CUDA_HOME
